04beff24768e4dc2bc6cb441245e6dc680cff8d3
83rifai <83rifai@gmail.com>
2016-10-06 20:07:24 +0700

    update

---
+++ application/views/template.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?=$this->pageTitle;?> :: SIKeu</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" media="screen" href="<?=base_url();?>assets/css/bootstrap.min.css">
+        <link rel="stylesheet" media="screen" href="<?=base_url();?>assets/css/bootstrap-theme.min.css">
+
+        <!-- Bootstrap Admin Theme -->
+        <link rel="stylesheet" media="screen" href="<?=base_url();?>assets/css/bootstrap-admin-theme.css">
+        <link rel="stylesheet" media="screen" href="<?=base_url();?>assets/css/bootstrap-admin-theme-change-size.css">
+
+        <!-- Vendors -->
+        <link rel="stylesheet" media="screen" href="<?=base_url();?>assets/vendors/easypiechart/jquery.easy-pie-chart.css">
+        <link rel="stylesheet" media="screen" href="<?=base_url();?>assets/vendors/easypiechart/jquery.easy-pie-chart_custom.css">
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+           <script type="text/javascript" src="js/html5shiv.js"></script>
+           <script type="text/javascript" src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body class="bootstrap-admin-with-small-navbar">
+		<input type="hidden" id="base_url" value="<?=base_url();?>" />
+        <!-- small navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top bootstrap-admin-navbar-sm" role="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav navbar-left bootstrap-admin-theme-change-size">
+                                <li class="text">Change size:</li>
+                                <li><a class="size-changer small">Small</a></li>
+                                <li><a class="size-changer large active">Large</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li>
+                                    <a href="#">Reminders <i class="glyphicon glyphicon-bell"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#">Settings <i class="glyphicon glyphicon-cog"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#">Go to frontend <i class="glyphicon glyphicon-share-alt"></i></a>
+                                </li>
+                                <li class="dropdown" >
+                                    <a href="#" role="button" class="dropdown-toggle" data-hover="dropdown"> <i class="glyphicon glyphicon-user"></i> Username <i class="caret"></i></a>
+                                    <ul class="dropdown-menu"style="background-color: #152F49;">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li role="presentation" class="divider"></li>
+                                        <li><a href="index.html">Logout</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- main / large navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top bootstrap-admin-navbar bootstrap-admin-navbar-under-small" role="navigation">
+            <!-- menus -->
+			<div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="<?=base_url();?>">Sistem Integrasi Keuangan</a>
+                        </div>
+						<!-- menus -->
+						<?php $this->load->view('partials/menus');?>
+                        <!-- end -->
+                    </div>
+                </div>
+            </div>
+			<!-- end -->
+			<!-- /.container -->
+        </nav>
+
+        <div class="main-content container" >
+            <!-- left, vertical navbar & content -->
+            <div class="row">
+                <!-- left, vertical navbar -->
+                
+
+                <!-- content -->
+                <div class="col-md-12">
+                    <?php $this->load->view('partials/breadcrumbs');?>
+					<!-- for notification -->
+					<div class="row">
+                        <div class="col-lg-12">
+                            <div class="alert alert-success bootstrap-admin-alert">
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+                                <h4>Success</h4>
+                                The operation completed successfully
+                            </div>
+                        </div>
+                    </div>
+					<!-- end -->
+					
+                    
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel panel-default bootstrap-admin-no-table-panel">
+                                <div class="panel-heading">
+                                    <div class="text-muted bootstrap-admin-box-title">Statistics</div>
+                                    <div class="pull-right"><span class="badge">View More</span></div>
+                                </div>
+                                <div class="bootstrap-admin-panel-content bootstrap-admin-no-table-panel-content collapse in">
+                                    <div class="col-md-3">
+                                        <div class="easyPieChart" data-percent="73" style="width: 110px; height: 110px; line-height: 110px;">73%<canvas width="110" height="110"></canvas></div>
+                                        <div class="chart-bottom-heading"><span class="label label-info">Visitors</span></div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="easyPieChart" data-percent="53" style="width: 110px; height: 110px; line-height: 110px;">53%<canvas width="110" height="110"></canvas></div>
+                                        <div class="chart-bottom-heading"><span class="label label-info">Page Views</span></div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="easyPieChart" data-percent="83" style="width: 110px; height: 110px; line-height: 110px;">83%<canvas width="110" height="110"></canvas></div>
+                                        <div class="chart-bottom-heading"><span class="label label-info">Users</span></div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="easyPieChart" data-percent="13" style="width: 110px; height: 110px; line-height: 110px;">13%<canvas width="110" height="110"></canvas></div>
+                                        <div class="chart-bottom-heading"><span class="label label-info">Orders</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                                        
+                </div>
+            </div>
+        </div>
+
+        <!-- footer -->
+		<?php $this->load->view('partials/footer');?>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script type="text/javascript" src="<?=base_url();?>assets/js/jquery-2.0.3.min.js"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?=base_url();?>assets/js/twitter-bootstrap-hover-dropdown.min.js"></script>
+        <script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap-admin-theme-change-size.js"></script>
+        <script type="text/javascript" src="<?=base_url();?>assets/vendors/easypiechart/jquery.easy-pie-chart.js"></script>
+
+        <script type="text/javascript">
+            $(function() {
+                // Easy pie charts
+                $('.easyPieChart').easyPieChart({animate: 1000});
+            });
+        </script>
+    </body>
+</html>
