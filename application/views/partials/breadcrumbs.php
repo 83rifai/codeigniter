98b5c3cfa4da9d1d936eb1a2efa32813f9fc36c2
83rifai <83rifai@gmail.com>
2017-01-11 11:43:49 +0700

    Signed-off-by: 83rifai <83rifai@gmail.com>

---
+++ application/views/partials/breadcrumbs.php
@@ -0,0 +1,15 @@
+	<div class="row">
+                        <div class="col-lg-12">
+                            <div class="navbar navbar-default bootstrap-admin-navbar-thin">
+                                <ol class="breadcrumb bootstrap-admin-breadcrumb">
+                                    <li>
+                                        <a href="#">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Settings</a>
+                                    </li>
+                                    
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
---
+++ application/views/partials/footer.php
@@ -0,0 +1,12 @@
+<div class="navbar navbar-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <footer role="contentinfo">
+                    <p class="left">Template :: Bootstrap Framework</p>
+                    <p class="right">&copy; <?= date('Y') ?> <a href="http://www.codeigniter.com" target="_blank">Codeigniter</a></p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</div>
